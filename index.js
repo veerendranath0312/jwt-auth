2a19f054a4c07c307ebb94d73c99336a4085ccbb
Veerendranath <veerendranath0312@gmail.com>
2023-05-17 21:35:21 +0530

    Update register function

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const connectDB = require('./db.js')
+const authRouter = require('./routes/auth.router.js')
+const middleware = require('./utils/middleware.js')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json()) // parse incoming JSON payload
 
+app.use('/api/auth', authRouter)
+
+app.use(middleware.errorHandler)
+
 const start = async () => {
   try {
-    await connectDB(`mongodb://localhost:27017/role_auth`)
+    await connectDB(`mongodb://127.0.0.1:27017/role_auth`)
     app.listen(8080, () => {
       console.log('Server connected to port 8080')
     })
